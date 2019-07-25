f4fea9fb4de7904e8e349cb586b224697216dc66
Stefan Kürzeder <stefan.kuerzeder@whiteduck.de>
2019-07-25 12:56:49 +0200

    Improved Utils::parse_named_table

--- module/Utils.h
+++ module/Utils.h
@@ -5,25 +5,50 @@
 
 #include "ILuaModuleManager.h"
 
+static const int index_value = -1;
+static const int index_key   = -2;
+
 class Utils
 {
 public:
-	static inline std::unordered_map<std::string, std::string> parse_named_table(lua_State* lua_vm, const int index) // Can only parse lua tables with string indices, e.g. { ["a"] = 1, ["b"] = "g" }
+	static inline std::unordered_map<std::string, std::string> parse_named_table(lua_State* lua_vm, const int index)
 	{
 		unordered_map<std::string, std::string> result;
 
-		lua_pushnil(lua_vm);
-		while(lua_next(lua_vm, index) != 0)
+		for(lua_pushnil(lua_vm); lua_next(lua_vm, index) != 0; lua_pop(lua_vm, 1))
 		{
-			if (lua_type(lua_vm, -1) != LUA_TSTRING || lua_type(lua_vm, -2) != LUA_TSTRING)
+			const auto type_key   = lua_type(lua_vm, index_key);
+			const auto type_value = lua_type(lua_vm, index_value);
+			if (type_key != LUA_TSTRING && type_key != LUA_TNUMBER)
 			{
 				stringstream ss;
-				ss << "got invalid table entry, expected key(string) and value(string), but was key(" << lua_typename(lua_vm, lua_type(lua_vm, -2)) << ") and value(" << lua_typename(lua_vm, lua_type(lua_vm, -1)) << ")";
+				ss << "got invalid table key, expected string or number got " << lua_typename(lua_vm, type_key);
 				throw runtime_error(ss.str());
 			}
 
-			result[lua_tostring(lua_vm, -2)] = lua_tostring(lua_vm, -1);
-			lua_pop(lua_vm, 1);
+			if (type_value != LUA_TSTRING && type_value != LUA_TNUMBER)
+			{
+				stringstream ss;
+				ss << "got invalid table value, expected string or number got " << lua_typename(lua_vm, type_value);
+				throw runtime_error(ss.str());
+			}
+
+			auto to_string = [lua_vm](const int index) -> const std::string
+			{
+				const auto type = lua_type(lua_vm, index);
+				if (type == LUA_TSTRING)
+				{
+					return lua_tostring(lua_vm, index);
+				}
+
+				if (type == LUA_TNUMBER)
+				{
+					return std::to_string(lua_tointeger(lua_vm, index));
+				}
+				return {};
+			};
+
+			result[to_string(index_key)] = to_string(index_value);
 		}
 
 		return result;
